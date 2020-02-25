8eaa18bdff6eba80894921ba4ea5ac821c3012b3
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-02-25 13:37:24 -0500

    Enabled migrations

---
+++ RestaurantRater/Migrations/202002251652066_InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿namespace RestaurantRater.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Restaurants",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FoodStyle = c.String(nullable: false),
+                        Name = c.String(nullable: false),
+                        Rating = c.Single(nullable: false),
+                        DollarSigns = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Restaurants");
+        }
+    }
+}
