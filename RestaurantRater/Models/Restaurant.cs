d5ce423f5651ebe34d066c45f1ac3b20c3ee3205
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-02-25 10:44:41 -0500

    Add project files.

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        // ID is the Primary Key
+        // A key is a required field by default
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public string FoodStyle { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        [Range(0, 5)]
+        public float Rating { get; set; }
+        
+        [Required]
+        // Only allows the values between 1-5 to be a valid input for the property
+        [Range(1, 5)]
+        public int DollarSigns { get; set; }
+    }
+}
---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
